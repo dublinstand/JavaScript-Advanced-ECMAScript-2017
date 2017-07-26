cbdc4132b26b519520644126ebe10ed639794337
Deyan <dublinstand@gmail.com>
2017-07-26 15:33:45 +0100

    init

---
+++ 04-Object-Composition/Lab/02_fibonacciNumbersWithClosure.js
@@ -0,0 +1,27 @@
+//You are given a number n, return an array containing the 1st to nth Fibonacci number.
+
+
+function fibonacciNumbersWithClosure(n){
+    let fibArray = [];
+
+    for (let i = 0; i < n; i++) {
+        fibArray.push(fib());
+    }
+
+    return fibArray;
+}
+
+let fib = (() => {
+    let fib1 = 0;
+    let fib2 = 1;
+
+    return () => {
+        let oldf1 = fib1;
+        let oldf2 = fib2;
+        fib1 = oldf2;
+        fib2 = oldf1 + oldf2;
+        return fib1;
+    }
+})();
+
+console.log(fibonacciNumbersWithClosure(8));
---
+++ 04-Object-Composition/Lab/03_listProcessor.js
@@ -0,0 +1,35 @@
+//Using a closure, create an inner object to process list commands. The commands supported should be the following:
+//•	add <string> - adds the following string in an inner collection.
+//•	remove <string> - removes all occurrences of the supplied <string> from the inner collection.
+//•	print - prints all elements of the inner collection joined by ", ".
+//Input
+//The input will come as an array of strings - each string represents a command to be executed from the command execution engine.
+//Output
+//For every print command - you should print on the console the inner collection joined by ", "
+
+
+function listProcessor(input){
+    let commandProcessor = (function(){
+        let list = [];
+
+        //here are our functions that are returned to commandProcessor
+        return {
+            add: (newItem) => list.push(newItem),
+
+            //filter and return all elements that are different then the item given (remove the item)
+            remove: (item) => list = list.filter(x => x != item),
+
+            print: () => console.log(list)
+        }
+    })();
+
+    for(let cmd of input){
+        let [action, data] = cmd.split(' ');
+        commandProcessor[action](data);
+    }
+}
+
+listProcessor(['add hello', 'add again', 'remove hello', 'add again', 'print']);
+listProcessor(['add pesho', 'add gosho', 'add pesho', 'remove pesho','print']);
+
+
